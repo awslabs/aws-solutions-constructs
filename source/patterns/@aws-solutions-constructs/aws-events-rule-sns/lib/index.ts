fbbfe7652306eabb0f5225250002738155b121cf
Nikhil Reddy <reddygun@amazon.com>
2020-08-31 15:01:21 -0400

    changes to the implementation of the constructs and tests

--- source/patterns/@aws-solutions-constructs/aws-events-rule-sns/lib/index.ts
+++ source/patterns/@aws-solutions-constructs/aws-events-rule-sns/lib/index.ts
@@ -21,12 +21,6 @@ import { Effect } from '@aws-cdk/aws-iam';
 
 export interface EventsRuleToSNSTopicProps {
     /**
-     * Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.
-     *
-     * @default - None
-     */
-    readonly existingSNSTopic?: sns.Topic,
-    /**
      * User provided props to override the default props for the SNS Topic.
      *
      * @default - Default props are used
@@ -38,44 +32,67 @@ export interface EventsRuleToSNSTopicProps {
      * @default - None
      */
     readonly eventRuleProps: events.RuleProps
+    /**
+     * Use a KMS Key, either managed by this CDK app, or imported. If importing an encryption key, it must be specified in
+     * the encryptionKey property for this construct.
+     *
+     * @default - true (encryption enabled, managed by this CDK app).
+     */
+    readonly enableEncryption?: boolean
+    /**
+     * An optional, imported encryption key to encrypt the SNS topic with.
+     *
+     * @default - not specified.
+     */
+    readonly encryptionKey?: kms.Key
 }
 
 export class EventsRuleToSNSTopic extends Construct {
-
-    public readonly snsTopic: [sns.Topic, kms.Key];
+    public readonly snsTopic: sns.Topic;
     public readonly eventsRule: events.Rule;
-    public readonly key: kms.Key
+    public readonly encryptionKey: kms.Key
 
+
+    /**
+     * @summary Constructs a new instance of the EventRuleToSns class.
+     * @param {cdk.App} scope - represents the scope for all the resources.
+     * @param {string} id - this is a a scope-unique id.
+     * @param {EventsRuleToSNSTopicProps} props - user provided props for the construct.
+     * @since 0.8.0
+     * @access public
+     */
     constructor(scope: Construct, id: string, props: EventsRuleToSNSTopicProps) {
         super(scope, id);
 
-        //TODO if the existingSNSTopic is undefined or null then only then use the buildTopic to create a set of Topic and Key?? Confirm this functionality.
-        this.snsTopic = defaults.buildTopic(this, {
-            topicProps: props.snsTopicProps
+        //Setup the sns topic.
+        [this.snsTopic, this.encryptionKey] = defaults.buildTopic(this, {
+            topicProps: props.snsTopicProps,
+            enableEncryption: props.enableEncryption,
+            encryptionKey: props.encryptionKey
         });
 
+        //Setup the event rule target as sns topic.
         const topicEventTarget: events.IRuleTarget = {
             bind: () => ({
                 id: '',
-                arn: this.snsTopic[0].topicArn
+                arn: this.snsTopic.topicArn
             })
         }
 
+        //Setup up the event rule property.
         const defaultEventsRuleProps = defaults.DefaultEventsRuleProps([topicEventTarget]); 
         const eventsRuleProps = overrideProps(defaultEventsRuleProps, props.eventRuleProps, true);
 
+        //Setup up the event rule.
         this.eventsRule = new events.Rule(this, 'EventsRule', eventsRuleProps);
 
-        //Policy for event to be able to publish to the sns topic
-        const publishPolicy = new iam.PolicyStatement()
-        publishPolicy.addResources(this.snsTopic[0].topicArn)
-        publishPolicy.effect = Effect.ALLOW
-        publishPolicy.addActions('sns:Publish')
-        publishPolicy.addServicePrincipal('events.amazonaws.com')
-
-        this.snsTopic[0].addToResourcePolicy(publishPolicy)
-
-        //TODO add logic for encryption using KMS. and also confirm if there needs to be an option to either enable or disable KMS Encryption.
+        //Setup up the grant policy for event to be able to publish to the sns topic.
+        this.snsTopic.addToResourcePolicy(new iam.PolicyStatement({
+            resources: [this.snsTopic.topicArn],
+            actions: ['sns:Publish'],
+            principals: [new iam.ServicePrincipal('events.amazonaws.com')],
+            effect: Effect.ALLOW
+        }))
     }
 
 }
