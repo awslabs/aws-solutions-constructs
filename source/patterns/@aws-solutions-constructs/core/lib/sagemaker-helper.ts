236e48bf39a0e5fb2666e6e6fde31c23b54b9bdf
mickychetta <mickychetta@gmail.com>
2020-09-18 20:00:09 -0700

    fix(aws-lambda-sagemaker): Added security group and fixed port

--- source/patterns/@aws-solutions-constructs/core/lib/sagemaker-helper.ts
+++ source/patterns/@aws-solutions-constructs/core/lib/sagemaker-helper.ts
@@ -65,7 +65,8 @@ export function buildSagemakerNotebook(scope: cdk.Construct, _roleArn: string, p
   // Setup the notebook properties
   let sagemakerNotebookProps;
   let vpcInstance: ec2.Vpc;
-  let securityGroup: ec2.SecurityGroup;
+  let egressSecurityGroup: ec2.SecurityGroup;
+  let ingressSecurityGroup: ec2.SecurityGroup;
 
   // Conditional Sagemaker Notebook creation
   if (!props?.existingNotebookObj) {
@@ -103,14 +104,28 @@ export function buildSagemakerNotebook(scope: cdk.Construct, _roleArn: string, p
           });
 
           sagemakerNotebookProps.subnetId = vpcInstance.isolatedSubnets[0].subnetId;
-          securityGroup = new ec2.SecurityGroup(scope, "SecurityGroup", {
+
+          ingressSecurityGroup = new ec2.SecurityGroup(scope, 'ingress-security-group', {
             vpc: vpcInstance,
-            allowAllOutbound: false
+            allowAllOutbound: false,
+            securityGroupName: 'IngressSecurityGroup',
           });
+        
+          ingressSecurityGroup.addIngressRule(ec2.Peer.ipv4('10.0.0.0/16'), ec2.Port.tcp(3306));
+        
+          egressSecurityGroup = new ec2.SecurityGroup(scope, "SecurityGroup", {
+            vpc: vpcInstance,
+            allowAllOutbound: false,
+            securityGroupName: 'EgressSecurityGroup'
+          });
+
+          egressSecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
+
+
 
           vpcInstance.addFlowLog('FlowLog');
 
-          sagemakerNotebookProps.securityGroupIds = [securityGroup.securityGroupId];
+          sagemakerNotebookProps.securityGroupIds = [ingressSecurityGroup.securityGroupId, egressSecurityGroup.securityGroupId];
         }
       }
 
