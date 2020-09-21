5a0312cdf2173ef4f0ad95f9bf5aa0c050e8d06a
mickychetta <mickychetta@gmail.com>
2020-09-20 22:54:14 -0700

    fix(aws-lambda-sagemaker): Delete egress rule

--- source/patterns/@aws-solutions-constructs/core/lib/sagemaker-helper.ts
+++ source/patterns/@aws-solutions-constructs/core/lib/sagemaker-helper.ts
@@ -108,12 +108,10 @@ export function buildSagemakerNotebook(scope: cdk.Construct, _roleArn: string, p
             vpc: vpcInstance,
             allowAllOutbound: false
           });
-          ingressSecurityGroup.addIngressRule(ec2.Peer.ipv4('10.0.0.0/24'), ec2.Port.tcp(443));
           egressSecurityGroup = new ec2.SecurityGroup(scope, "egress-security-group", {
             vpc: vpcInstance,
             allowAllOutbound: false
           });
-          egressSecurityGroup.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
           vpcInstance.addFlowLog('FlowLog');
           sagemakerNotebookProps.securityGroupIds = [ingressSecurityGroup.securityGroupId, egressSecurityGroup.securityGroupId];
         }
