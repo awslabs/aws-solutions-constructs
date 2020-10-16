81d3b8b8ed9eabd4f2adbdc63d607975990c62ea
Daniel <3842788+dscpinheiro@users.noreply.github.com>
2020-10-16 12:59:22 +0000

    feat(aws-apigateway-sagemakerendpoint): New aws-apigateway-sagemakerendpoint pattern implementation (#87)
    * feat(aws-apigateway-sagemakerendpoint): New aws-apigateway-sagemakerendpoint pattern implementation
    
    * Update comment not to exceed line limit
    
    * Update construct to allow usage of existing role for API Gateway
    
    * Remove duplicated property
    
    * Add comment to clarify request validation

---
+++ source/patterns/@aws-solutions-constructs/aws-apigateway-sagemakerendpoint/test/apigateway-sagemakerendpoint.test.ts
@@ -0,0 +1,143 @@
+/**
+ *  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+// Imports
+import { Stack, Aws } from '@aws-cdk/core';
+import { ApiGatewayToSageMakerEndpoint } from '../lib';
+import * as iam from '@aws-cdk/aws-iam';
+import { SynthUtils } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest';
+
+// --------------------------------------------------------------
+// Test minimal deployment snapshot
+// --------------------------------------------------------------
+test('Test minimal deployment snapshot', () => {
+    const stack = new Stack();
+    new ApiGatewayToSageMakerEndpoint(stack, 'api-gateway-sagemakerendpoint', {
+        endpointName: 'my-endpoint',
+        resourcePath: '{my_param}',
+        requestMappingTemplate: 'my-request-vtl-template'
+    });
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+});
+
+// --------------------------------------------------------------
+// Test construct properties
+// --------------------------------------------------------------
+test('Test construct properties', () => {
+    const stack = new Stack();
+    const pattern = new ApiGatewayToSageMakerEndpoint(stack, 'api-gateway-sagemakerendpoint', {
+        endpointName: 'my-endpoint',
+        resourcePath: '{my_param}',
+        requestMappingTemplate: 'my-request-vtl-template'
+    });
+
+    expect(pattern.apiGateway !== null);
+    expect(pattern.apiGatewayRole !== null);
+    expect(pattern.apiGatewayCloudWatchRole !== null);
+    expect(pattern.apiGatewayLogGroup !== null);
+});
+
+// --------------------------------------------------------------
+// Test deployment w/ overwritten properties
+// --------------------------------------------------------------
+test('Test deployment w/ overwritten properties', () => {
+    const stack = new Stack();
+
+    const existingRole = new iam.Role(stack, 'api-gateway-role', {
+        assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+        description: 'existing role for SageMaker integration',
+        inlinePolicies: {
+            InvokePolicy: new iam.PolicyDocument({
+                statements: [new iam.PolicyStatement({
+                    resources: [`arn:${Aws.PARTITION}:sagemaker:${Aws.REGION}:${Aws.ACCOUNT_ID}:endpoint/my-endpoint`],
+                    actions: ['sagemaker:InvokeEndpoint']
+                })]
+            })
+        }
+    });
+
+    new ApiGatewayToSageMakerEndpoint(stack, 'api-gateway-sagemakerendpoint', {
+        endpointName: 'my-endpoint',
+        resourcePath: '{my_param}',
+        requestMappingTemplate: 'my-request-vtl-template',
+
+        apiGatewayProps: {
+            restApiName: 'my-api',
+            deployOptions: {
+                methodOptions: {
+                    '/*/*': {
+                        throttlingRateLimit: 100,
+                        throttlingBurstLimit: 25
+                    }
+                }
+            }
+        },
+        apiGatewayExecutionRole: existingRole,
+        resourceName: 'my-resource',
+        responseMappingTemplate: 'my-response-vtl-template'
+    });
+
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+
+    expect(stack).toHaveResourceLike('AWS::ApiGateway::Stage', {
+        MethodSettings: [
+            {
+                DataTraceEnabled: true,
+                HttpMethod: '*',
+                LoggingLevel: 'INFO',
+                ResourcePath: '/*'
+            },
+            {
+                HttpMethod: '*',
+                ResourcePath: '/*',
+                ThrottlingRateLimit: 100,
+                ThrottlingBurstLimit: 25
+            }
+        ]
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGateway::Resource', {
+        PathPart: 'my-resource'
+    });
+
+    expect(stack).toHaveResourceLike('AWS::ApiGateway::Method', {
+        Integration: {
+            IntegrationResponses: [
+                {
+                    ResponseTemplates: {
+                        'application/json': 'my-response-vtl-template',
+                    },
+                    StatusCode: '200'
+                },
+                {
+                    StatusCode: '500',
+                    SelectionPattern: '5\\d{2}'
+                },
+                {
+                    StatusCode: '400',
+                    SelectionPattern: '4\\d{2}'
+                }
+            ]
+        },
+        MethodResponses: [
+            { StatusCode: '200' },
+            { StatusCode: '500' },
+            { StatusCode: '400' }
+        ]
+    });
+
+    expect(stack).toHaveResourceLike('AWS::IAM::Role', {
+        Description: 'existing role for SageMaker integration'
+    });
+});
