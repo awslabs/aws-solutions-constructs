5ebf8e9888be86cbb3903c285509678dd2027d53
Beomseok Lee <beomseok@amazon.com>
2020-12-02 16:10:07 -0800

    feat(aws-cloudfront-mediastore): new aws-cloudfront-mediastore pattern implementation

--- source/patterns/@aws-solutions-constructs/core/lib/cloudfront-distribution-defaults.ts
+++ source/patterns/@aws-solutions-constructs/core/lib/cloudfront-distribution-defaults.ts
@@ -16,6 +16,7 @@ import * as origins from '@aws-cdk/aws-cloudfront-origins';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as api from '@aws-cdk/aws-apigateway';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as mediastore from '@aws-cdk/aws-mediastore';
 import * as cdk from '@aws-cdk/core';
 
 export function DefaultCloudFrontWebDistributionForApiGatewayProps(apiEndPoint: api.RestApi,
@@ -89,4 +90,28 @@ export function DefaultCloudFrontWebDistributionForS3Props(sourceBucket: s3.Buck
             defaultRootObject: 'index.html'
         } as cloudfront.DistributionProps;
     }
+}
+
+export function DefaultCloudFrontDisributionForMediaStoreProps(mediastoreContainer: mediastore.CfnContainer,
+                                                               loggingBucket: s3.Bucket,
+                                                               originRequestPolicy: cloudfront.OriginRequestPolicy,
+                                                               customHeaders?: Record<string, string>): cloudfront.DistributionProps {
+    const mediaStoreContainerUrlWithoutProtocol = cdk.Fn.select(1, cdk.Fn.split('://', mediastoreContainer.attrEndpoint));
+    const mediaStoreContainerDomainName = cdk.Fn.select(0, cdk.Fn.split('/', mediaStoreContainerUrlWithoutProtocol));
+
+    const httpOrigin: origins.HttpOrigin = customHeaders ?
+        new origins.HttpOrigin(mediaStoreContainerDomainName, { customHeaders }) :
+        new origins.HttpOrigin(mediaStoreContainerDomainName);
+
+    return {
+        defaultBehavior: {
+            origin: httpOrigin,
+            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+            cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+            originRequestPolicy
+        },
+        enableLogging: true,
+        logBucket: loggingBucket
+    } as cloudfront.DistributionProps;
 }
---
+++ source/patterns/@aws-solutions-constructs/core/lib/mediastore-helper.ts
@@ -0,0 +1,36 @@
+/**
+ *  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+import * as cdk from '@aws-cdk/core';
+import * as mediastore from '@aws-cdk/aws-mediastore';
+import { MediaStoreContainerProps } from './mediastore-defaults';
+import { overrideProps } from './utils';
+
+export function MediaStoreContainer(scope: cdk.Construct, mediaStoreContainerProps?: mediastore.CfnContainerProps): mediastore.CfnContainer {
+  const defaultprops: mediastore.CfnContainerProps = MediaStoreContainerProps();
+  let mediaStoreProps: mediastore.CfnContainerProps;
+
+  if (mediaStoreContainerProps) {
+    mediaStoreProps = overrideProps(defaultprops, mediaStoreContainerProps);
+  } else {
+    mediaStoreProps = defaultprops;
+  }
+
+  // Create the MediaStore Container
+  const mediaStoreContainer = new mediastore.CfnContainer(scope, 'MediaStoreContainer', mediaStoreProps);
+
+  // Add deletion policy to retain because it will fail to delete if the container is not empty.
+  mediaStoreContainer.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
+
+  return mediaStoreContainer;
+}
