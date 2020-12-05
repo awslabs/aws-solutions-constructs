a8f03e2a1802335824ae86ada0e24e7750719456
nihitkas <nihitkas@amazon.com>
2020-12-05 10:44:30 -0500

    adding unit tests

---
+++ source/patterns/@aws-solutions-constructs/aws-kinesisstream-gluejob/test/integ.existing-job.ts
@@ -0,0 +1,48 @@
+/**
+ *  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+// Imports
+import { CfnJob } from '@aws-cdk/aws-glue';
+import { Role, ServicePrincipal } from '@aws-cdk/aws-iam';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { App, Duration, Stack } from '@aws-cdk/core';
+import { KinesisStreamGlueJob } from '../lib';
+
+// Setup
+const app = new App();
+const stack = new Stack(app, 'test-kinesisstream-gluejob');
+stack.templateOptions.description = 'Integration Test for aws-kinesisstream-gluejob';
+
+const job = new CfnJob(stack, 'ExistingJob', {
+    command: {
+        name: 'existingJobName',
+        pythonVersion: '3',
+        scriptLocation: new Bucket(stack, 'existingScriptLocation', {
+            versioned: false,
+            lifecycleRules: [{
+                expiration: Duration.days(30)
+            }]
+        }).bucketArn,
+    },
+    role: new Role(stack, 'JobRole', {
+        assumedBy: new ServicePrincipal('glue.amazonaws.com')
+    }).roleArn
+});
+
+// Definitions
+new KinesisStreamGlueJob(stack, 'test-kinesisstreams-lambda', {
+    existingGlueJob: job
+});
+
+// Synth
+app.synth();
--- source/patterns/@aws-solutions-constructs/aws-kinesisstream-gluejob/test/test.kinesisstream-gluejob.test.ts
+++ source/patterns/@aws-solutions-constructs/aws-kinesisstream-gluejob/test/test.kinesisstream-gluejob.test.ts
@@ -13,7 +13,10 @@
 
 import { ResourcePart, SynthUtils } from '@aws-cdk/assert';
 import '@aws-cdk/assert/jest';
+import { CfnJob } from '@aws-cdk/aws-glue';
+import { Bucket } from '@aws-cdk/aws-s3';
 import { Stack } from "@aws-cdk/core";
+import { buildKinesisStream } from '@aws-solutions-constructs/core';
 import { KinesisStreamGlueJob, KinesisStreamGlueJobProps } from '../lib';
 
 // --------------------------------------------------------------
@@ -23,12 +26,11 @@ test('Pattern minimal deployment', () => {
     // Initial setup
     const stack = new Stack();
     const props: KinesisStreamGlueJobProps = {
-    glueJobProps: {
-        command: KinesisStreamGlueJob.createGlueJobCommand(stack, 'testJob', '3', undefined, undefined),
-        role: KinesisStreamGlueJob.createGlueJobRole(stack).roleArn,
-        securityConfiguration: 'testSecConfig'
-    }
-
+        glueJobProps: {
+            command: KinesisStreamGlueJob.createGlueJobCommand(stack, 'testJob', '3'),
+            role: KinesisStreamGlueJob.createGlueJobRole(stack).roleArn,
+            securityConfiguration: 'testSecConfig'
+        }
     };
     new KinesisStreamGlueJob(stack, 'test-kinesisstreams-lambda', props);
     // Assertion 1
@@ -116,4 +118,181 @@ test('Pattern minimal deployment', () => {
             Version: "2012-10-17"
         }
     }, ResourcePart.Properties);
+});
+
+// --------------------------------------------------------------
+// Test if existing Glue Job is provided
+// --------------------------------------------------------------
+test('Pattern minimal deployment', () => {
+    // Initial setup
+    const stack = new Stack();
+    const existingCfnJob = new CfnJob(stack, 'ExistingJob', {
+        command: KinesisStreamGlueJob.createGlueJobCommand(stack, 'testJob', '3'),
+        role: KinesisStreamGlueJob.createGlueJobRole(stack).roleArn,
+        securityConfiguration: 'testSecConfig'
+    });
+
+    new KinesisStreamGlueJob(stack, 'test-kinesisstreams-lambda', {
+        existingGlueJob: existingCfnJob
+    });
+    // Assertion 1
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+
+    // check for Kinesis Stream
+    expect(stack).toHaveResourceLike('AWS::Kinesis::Stream', {
+        Properties: {
+            RetentionPeriodHours: 24,
+            ShardCount: 1,
+            StreamEncryption: {
+                EncryptionType: "KMS",
+                KeyId: "alias/aws/kinesis",
+            },
+        },
+        Type: "AWS::Kinesis::Stream"
+    }, ResourcePart.CompleteDefinition);
+
+    // check policy to allow read access to Kinesis Stream
+    expect(stack).toHaveResourceLike('AWS::IAM::Policy', {
+        PolicyDocument: {
+            Statement: [{
+                Action: [
+                    "kinesis:DescribeStreamSummary",
+                    "kinesis:GetRecords",
+                    "kinesis:GetShardIterator",
+                    "kinesis:ListShards",
+                    "kinesis:SubscribeToShard",
+                ],
+                Effect: "Allow",
+                Resource: {
+                    "Fn::GetAtt": [
+                        "testkinesisstreamslambdaKinesisStream374D6D56",
+                        "Arn",
+                    ],
+                },
+            }],
+            Version: "2012-10-17"
+        }
+    }, ResourcePart.Properties);
+});
+
+// --------------------------------------------------------------
+// Test if existing S3 bucket location is provided
+// --------------------------------------------------------------
+test('When S3 bucket location for script exists', () => {
+    // Initial setup
+    const stack = new Stack();
+    const props: KinesisStreamGlueJobProps = {
+        glueJobProps: {
+            command: KinesisStreamGlueJob.createGlueJobCommand(stack, 'testJob', '3', new Bucket(stack, 'ScriptLocation')),
+            role: KinesisStreamGlueJob.createGlueJobRole(stack).roleArn,
+            securityConfiguration: 'testSecConfig'
+        },
+    };
+    new KinesisStreamGlueJob(stack, 'test-kinesisstreams-lambda', props);
+    // Assertion 1
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+
+    // Assert existing bucket
+    expect(stack).toHaveResourceLike('AWS::S3::Bucket', {
+        DeletionPolicy: "Retain",
+        Type: "AWS::S3::Bucket",
+        UpdateReplacePolicy: "Retain",
+    }, ResourcePart.CompleteDefinition);
+
+    // Assert bucket policy on existing bucket
+    expect(stack).toHaveResourceLike('AWS::S3::BucketPolicy', {
+        Properties: {
+            Bucket: {
+                Ref: "ScriptLocation6396DB53",
+            },
+            PolicyDocument: {
+                Statement: [{
+                    Action: [ "s3:GetObject*", "s3:GetBucket*", "s3:List*" ],
+                    Effect: "Allow",
+                    Principal: {
+                        Service: "glue.amazonaws.com",
+                    },
+                    Resource: [{
+                        "Fn::GetAtt": [
+                            "ScriptLocation6396DB53",
+                            "Arn",
+                        ]}, {
+                            "Fn::Join": [
+                                "", [{
+                                    "Fn::GetAtt": [
+                                        "ScriptLocation6396DB53",
+                                        "Arn",
+                                    ],
+                                },
+                                "/*",
+                                ],
+                            ],
+                        },
+                    ],
+                }],
+                Version: "2012-10-17",
+            }
+        },
+        Type: "AWS::S3::BucketPolicy"
+    }, ResourcePart.CompleteDefinition);
+});
+
+// --------------------------------------------------------------
+// Test when the construct is supplied with an existing stream
+// --------------------------------------------------------------
+test('create glue job with existing kinesis stream', () => {
+    const stack = new Stack();
+    const kinesisStream = buildKinesisStream(stack, {});
+    new KinesisStreamGlueJob(stack, 'existingStreamJob', {
+        existingStreamObj: kinesisStream,
+        glueJobProps: {
+            command: KinesisStreamGlueJob.createGlueJobCommand(stack, 'JobCommand', '3'),
+            role: KinesisStreamGlueJob.createGlueJobRole(stack).roleArn
+        }
+    });
+
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+
+    // Assert if the Glue Job role has read access to the stream
+    expect(stack).toHaveResourceLike('AWS::IAM::Policy', {
+        Properties: {
+            PolicyDocument: {
+                Statement: [{
+                    Action: [ "kinesis:DescribeStreamSummary", "kinesis:GetRecords", "kinesis:GetShardIterator",
+                                "kinesis:ListShards", "kinesis:SubscribeToShard"
+                    ],
+                    Effect: "Allow",
+                    Resource: {
+                        "Fn::GetAtt":  [
+                            "KinesisStream46752A3E",
+                            "Arn",
+                        ]
+                    },
+                }],
+                Version: "2012-10-17",
+            },
+            PolicyName: "GlueJobRolePolicy422D4F2E",
+            Roles: [{
+                "Fn::Select": [
+                    1, {
+                        "Fn::Split": [
+                        "/", {
+                            "Fn::Select": [
+                                5, {
+                                    "Fn::Split": [
+                                    ":", {
+                                        "Fn::GetAtt": [
+                                            "JobRole014917C6",
+                                            "Arn",
+                                        ],
+                                    }]
+                                },
+                            ]
+                        },
+                    ],
+                }],
+            }],
+        },
+        Type: "AWS::IAM::Policy",
+    }, ResourcePart.CompleteDefinition);
 });
