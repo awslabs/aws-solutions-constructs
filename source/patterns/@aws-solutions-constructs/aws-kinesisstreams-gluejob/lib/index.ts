fc15d89accdcd095996469c5fdc4eb799ecb8d54
nihitkas <nihitkas@amazon.com>
2021-01-15 11:24:21 -0500

    refactoring code to match construct patterns

--- source/patterns/@aws-solutions-constructs/aws-kinesisstreams-gluejob/lib/index.ts
+++ source/patterns/@aws-solutions-constructs/aws-kinesisstreams-gluejob/lib/index.ts
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  Copyright 2020-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
  *  with the License. A copy of the License is located at
@@ -38,7 +38,6 @@ export interface KinesisStreamGlueJobProps {
   readonly glueJobProps?: CfnJobProps | any;
   /**
    * Existing GlueJob configuration. If this property is provided, any configuration provided through @glueJobProps
-   * and @glueJobCommandProps will be ignored.
    */
   readonly existingGlueJob?: CfnJob;
   /**
