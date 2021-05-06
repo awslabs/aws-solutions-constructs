ac9251cd0b6acf3e8eaae8e10b9a562f2fc0cd38
Allen Moheimani <72047367+ammoheim@users.noreply.github.com>
2021-05-06 10:38:56 -0700

    aws-lambda-secretsmanager (#162)
    * docs: added README.md and architecture.png
    
    * feat(aws-lambda-secretsmanager): Updated documentation with code review changes requested
    
    * feat(aws-lambda-secretsmanager): Updated documentation with code review changes requested
    
    * feat(aws-lambda-secretsmanager): Created core helpers to provision secrets manager secret.
    
    * feat(aws-lambda-secretsmanager): Fixed double quotes in import
    
    * feat(aws-lambda-secretsmanager): Fixed double quotes in import
    
    * implemented lib/index.ts; added ignore files; added package.json
    
    * fixed naming
    
    * feat(aws-lambda-secretsmanager): Implementation changes, integration tests, and unit tests
    
    * feat(aws-lambda-secretsmanager): Added missing snapshot
    
    * feat(aws-lambda-secretsmanager): Updated documentation and test snapshots.
    
    * feat(aws-lambda-secretsmanager): Updated documentation and test snapshots.
    
    * suppressed warning on build
    
    * docs: added README.md and architecture.png
    
    * implemented lib/index.ts; added ignore files; added package.json
    
    * feat(aws-lambda-secretsmanager): Updated documentation with code review changes requested
    
    * feat(aws-lambda-secretsmanager): Created core helpers to provision secrets manager secret.
    
    * feat(aws-lambda-secretsmanager): Fixed double quotes in import
    
    * feat(aws-lambda-secretsmanager): Fixed double quotes in import
    
    * fixed naming
    
    * feat(aws-lambda-secretsmanager): Implementation changes, integration tests, and unit tests
    
    * feat(aws-lambda-secretsmanager): Added missing snapshot
    
    * feat(aws-lambda-secretsmanager): Updated documentation and test snapshots.
    
    * feat(aws-lambda-secretsmanager): Updated documentation and test snapshots.
    
    * suppressed warning on build
    
    * feedback from PR
    
    * fixed failing test
    
    * feat(aws-lambda-secretsmanager): code review fixes.
    
    * feat(aws-lambda-secretsmanager): unit test code review fixes.
    
    * updated removal policy for tests to destroy the secret upon stack deletion
    
    * feat(aws-lambda-secretsmanager): Updated unit tests snapshot
    
    * feat(aws-lambda-secretsmanager): Updates to utilize secret ARN instead of name.
    
    * feat(aws-lambda-secretsmanager): Included unit test to override the secretProps and validate the KMS key permissions.
    
    * feat(aws-lambda-secretsmanager): Included viper light ignore for unit test.
    
    Co-authored-by: Daniel Matuki da Cunha <dmatuki@amazon.com>
    Co-authored-by: Daniel Matuki da Cunha <64283256+danielmatuki@users.noreply.github.com>
    Co-authored-by: Hitendra Nishar <hnishar@amazon.com>

---
+++ source/patterns/@aws-solutions-constructs/aws-lambda-secretsmanager/lib/index.ts
@@ -0,0 +1,139 @@
+/**
+ *  Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+// Imports
+import * as defaults from "@aws-solutions-constructs/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import { Construct } from "@aws-cdk/core";
+
+/**
+ * @summary The properties for the LambdaToSecretsmanager class.
+ */
+export interface LambdaToSecretsmanagerProps {
+  /**
+   * Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.
+   *
+   * @default - None
+   */
+  readonly existingLambdaObj?: lambda.Function;
+  /**
+   * User provided props to override the default props for the Lambda function.
+   *
+   * @default - Default properties are used.
+   */
+  readonly lambdaFunctionProps?: lambda.FunctionProps;
+  /**
+   * Existing instance of Secret object, if this is set then secretProps is ignored.
+   *
+   * @default - Default props are used
+   */
+  readonly existingSecretObj?: secretsmanager.Secret;
+  /**
+   * Optional user-provided props to override the default props for the Secret.
+   *
+   * @default - Default props are used
+   */
+  readonly secretProps?: secretsmanager.SecretProps;
+  /**
+   * An existing VPC for the construct to use (construct will NOT create a new VPC in this case)
+   */
+  readonly existingVpc?: ec2.IVpc;
+  /**
+   * Properties to override default properties if deployVpc is true
+   */
+  readonly vpcProps?: ec2.VpcProps;
+  /**
+   * Whether to deploy a new VPC
+   *
+   * @default - false
+   */
+  readonly deployVpc?: boolean;
+  /**
+   * Optional Name for the Secret environment variable set for the Lambda function.
+   *
+   * @default - SECRET_NAME
+   */
+  readonly secretEnvironmentVariableName?: string;
+  /**
+   * Optional secret permissions to grant to the Lambda function.
+   * One of the following may be specified: "Read" or "ReadWrite".
+   *
+   * @default - Read only acess is given to the Lambda function if no value is specified.
+   */
+  readonly grantWriteAccess?: string;
+}
+
+/**
+ * @summary The LambdaToSecretsmanager class.
+ */
+export class LambdaToSecretsmanager extends Construct {
+    public readonly lambdaFunction: lambda.Function;
+    public readonly secret: secretsmanager.Secret;
+    public readonly vpc?: ec2.IVpc;
+
+    /**
+     * @summary Constructs a new instance of the LambdaToSecretsmanager class.
+     * @param {cdk.App} scope - represents the scope for all the resources.
+     * @param {string} id - this is a a scope-unique id.
+     * @param {LambdaToSecretsmanagerProps} props - user provided props for the construct.
+     * @access public
+     */
+    constructor(scope: Construct, id: string, props: LambdaToSecretsmanagerProps) {
+      super(scope, id);
+
+      if (props.deployVpc || props.existingVpc) {
+        if (props.deployVpc && props.existingVpc) {
+          throw new Error("More than 1 VPC specified in the properties");
+        }
+
+        this.vpc = defaults.buildVpc(scope, {
+          defaultVpcProps: defaults.DefaultIsolatedVpcProps(),
+          existingVpc: props.existingVpc,
+          userVpcProps: props.vpcProps,
+          constructVpcProps: {
+            enableDnsHostnames: true,
+            enableDnsSupport: true,
+          },
+        });
+
+        defaults.AddAwsServiceEndpoint(scope, this.vpc, defaults.ServiceEndpointTypes.SECRETS_MANAGER);
+      }
+
+      // Setup the Lambda function
+      this.lambdaFunction = defaults.buildLambdaFunction(this, {
+        existingLambdaObj: props.existingLambdaObj,
+        lambdaFunctionProps: props.lambdaFunctionProps,
+        vpc: this.vpc,
+      });
+
+      // Setup the Secret
+      if (props.existingSecretObj) {
+        this.secret = props.existingSecretObj;
+      } else {
+        this.secret = defaults.buildSecretsManagerSecret(this, 'secret', props.secretProps);
+      }
+
+      // Configure environment variables
+      const secretEnvironmentVariableName = props.secretEnvironmentVariableName || 'SECRET_ARN';
+      this.lambdaFunction.addEnvironment(secretEnvironmentVariableName, this.secret.secretArn);
+
+      // Enable read permissions for the Lambda function by default
+      this.secret.grantRead(this.lambdaFunction);
+
+      if (props.grantWriteAccess === 'ReadWrite') {
+        this.secret.grantWrite(this.lambdaFunction);
+      }
+    }
+}
---
+++ source/patterns/@aws-solutions-constructs/aws-lambda-secretsmanager/test/lambda-secretsmanager.test.ts
@@ -0,0 +1,604 @@
+/**
+ *  Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+// Imports
+import { RemovalPolicy, Stack } from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { Secret } from '@aws-cdk/aws-secretsmanager';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import { LambdaToSecretsmanager } from '../lib';
+import { SynthUtils } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest';
+import * as defaults from "@aws-solutions-constructs/core";
+
+// --------------------------------------------------------------
+// Test minimal deployment with new Lambda function
+// --------------------------------------------------------------
+test('Test minimal deployment with new Lambda function', () => {
+  // Stack
+  const stack = new Stack();
+  // Helper declaration
+  new LambdaToSecretsmanager(stack, 'lambda-to-secretsmanager-stack', {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`)
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+  });
+  // Assertion 1
+  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+});
+
+// --------------------------------------------------------------
+// Test the getter methods
+// --------------------------------------------------------------
+test('Test the properties', () => {
+  // Stack
+  const stack = new Stack();
+  // Helper declaration
+  const pattern = new LambdaToSecretsmanager(stack, 'lambda-to-secretsmanager-stack', {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`)
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+  });
+  // Assertion 1
+  const func = pattern.lambdaFunction;
+  expect(func).toBeDefined();
+  // Assertion 2
+  const secret = pattern.secret;
+  expect(secret).toBeDefined();
+});
+
+// --------------------------------------------------------------
+// Test deployment w/ existing secret
+// --------------------------------------------------------------
+test('Test deployment w/ existing secret', () => {
+  // Stack
+  const stack = new Stack();
+  // Helper declaration
+  const existingSecret = new Secret(stack, 'secret', {});
+  const pattern = new LambdaToSecretsmanager(stack, 'lambda-to-secretsmanager-stack', {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`)
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+    existingSecretObj: existingSecret
+  });
+  // Assertion 1
+  expect(stack).toHaveResource("AWS::SecretsManager::Secret", {
+    GenerateSecretString: {},
+  });
+  // Assertion 2
+  expect(pattern.secret).toBe(existingSecret);
+});
+
+// --------------------------------------------------------------
+// Test deployment w/ existing function
+// --------------------------------------------------------------
+test('Test deployment w/ existing function', () => {
+  // Stack
+  const stack = new Stack();
+  // Helper declaration
+  const lambdaFunctionProps = {
+    runtime: lambda.Runtime.NODEJS_10_X,
+    handler: 'index.handler',
+    code: lambda.Code.fromAsset(`${__dirname}/lambda`)
+  };
+  const existingFuntion = defaults.deployLambdaFunction(stack, lambdaFunctionProps);
+
+  const pattern = new LambdaToSecretsmanager(stack, 'lambda-to-secretsmanager-stack', {
+    existingLambdaObj: existingFuntion,
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+  });
+  // Assertion 1
+  expect(stack).toHaveResource("AWS::SecretsManager::Secret", {
+    GenerateSecretString: {},
+  });
+  // Assertion 2
+  expect(pattern.lambdaFunction).toBe(existingFuntion);
+});
+
+// --------------------------------------------------------------
+// Test minimal deployment with write access to Secret
+// --------------------------------------------------------------
+test('Test minimal deployment write access to Secret', () => {
+  // Stack
+  const stack = new Stack();
+  // Helper declaration
+  new LambdaToSecretsmanager(stack, 'lambda-to-secretsmanager-stack', {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+    grantWriteAccess: 'ReadWrite'
+  });
+  // Assertion 1
+  expect(stack).toHaveResource("AWS::SecretsManager::Secret", {
+    GenerateSecretString: {},
+  });
+
+});
+
+// --------------------------------------------------------------
+// Test minimal deployment that deploys a VPC without vpcProps
+// --------------------------------------------------------------
+test("Test minimal deployment that deploys a VPC without vpcProps", () => {
+  // Stack
+  const stack = new Stack();
+  // Helper declaration
+  new LambdaToSecretsmanager(stack, "lambda-to-secretsmanager-stack", {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+    deployVpc: true,
+  });
+
+  expect(stack).toHaveResource("AWS::Lambda::Function", {
+    VpcConfig: {
+      SecurityGroupIds: [
+        {
+          "Fn::GetAtt": [
+            "lambdatosecretsmanagerstackReplaceDefaultSecurityGroupsecuritygroupED420155",
+            "GroupId",
+          ],
+        },
+      ],
+      SubnetIds: [
+        {
+          Ref: "VpcisolatedSubnet1SubnetE62B1B9B",
+        },
+        {
+          Ref: "VpcisolatedSubnet2Subnet39217055",
+        },
+      ],
+    },
+  });
+
+  expect(stack).toHaveResource("AWS::EC2::VPC", {
+    EnableDnsHostnames: true,
+    EnableDnsSupport: true,
+  });
+
+  expect(stack).toHaveResource("AWS::EC2::VPCEndpoint", {
+    VpcEndpointType: "Interface",
+  });
+
+  expect(stack).toCountResources("AWS::EC2::Subnet", 2);
+  expect(stack).toCountResources("AWS::EC2::InternetGateway", 0);
+});
+
+// --------------------------------------------------------------
+// Test minimal deployment that deploys a VPC w/vpcProps
+// --------------------------------------------------------------
+test("Test minimal deployment that deploys a VPC w/vpcProps", () => {
+  // Stack
+  const stack = new Stack();
+  // Helper declaration
+  new LambdaToSecretsmanager(stack, "lambda-to-secretsmanager-stack", {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+    vpcProps: {
+      enableDnsHostnames: false,
+      enableDnsSupport: false,
+      cidr: "192.68.0.0/16",
+    },
+    deployVpc: true,
+  });
+
+  expect(stack).toHaveResource("AWS::Lambda::Function", {
+    VpcConfig: {
+      SecurityGroupIds: [
+        {
+          "Fn::GetAtt": [
+            "lambdatosecretsmanagerstackReplaceDefaultSecurityGroupsecuritygroupED420155",
+            "GroupId",
+          ],
+        },
+      ],
+      SubnetIds: [
+        {
+          Ref: "VpcisolatedSubnet1SubnetE62B1B9B",
+        },
+        {
+          Ref: "VpcisolatedSubnet2Subnet39217055",
+        },
+      ],
+    },
+  });
+
+  expect(stack).toHaveResource("AWS::EC2::VPC", {
+    CidrBlock: "192.68.0.0/16",
+    EnableDnsHostnames: true,
+    EnableDnsSupport: true,
+  });
+
+  expect(stack).toHaveResource("AWS::EC2::VPCEndpoint", {
+    VpcEndpointType: "Interface",
+  });
+
+  expect(stack).toCountResources("AWS::EC2::Subnet", 2);
+  expect(stack).toCountResources("AWS::EC2::InternetGateway", 0);
+});
+
+// --------------------------------------------------------------
+// Test minimal deployment with an existing VPC
+// --------------------------------------------------------------
+test("Test minimal deployment with an existing VPC", () => {
+  // Stack
+  const stack = new Stack();
+
+  const testVpc = new ec2.Vpc(stack, "test-vpc", {});
+
+  // Helper declaration
+  new LambdaToSecretsmanager(stack, "lambda-to-secretsmanager-stack", {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+    existingVpc: testVpc,
+  });
+
+  expect(stack).toHaveResource("AWS::Lambda::Function", {
+    VpcConfig: {
+      SecurityGroupIds: [
+        {
+          "Fn::GetAtt": [
+            "lambdatosecretsmanagerstackReplaceDefaultSecurityGroupsecuritygroupED420155",
+            "GroupId",
+          ],
+        },
+      ],
+      SubnetIds: [
+        {
+          Ref: "testvpcPrivateSubnet1Subnet865FB50A",
+        },
+        {
+          Ref: "testvpcPrivateSubnet2Subnet23D3396F",
+        },
+      ],
+    },
+  });
+
+  expect(stack).toHaveResource("AWS::EC2::VPCEndpoint", {
+    VpcEndpointType: "Interface",
+  });
+});
+
+// --------------------------------------------------------------
+// Test minimal deployment with an existing VPC and existing Lambda function not in a VPCs
+//
+// buildLambdaFunction should throw an error if the Lambda function is not
+// attached to a VPC
+// --------------------------------------------------------------
+test("Test minimal deployment with an existing VPC and existing Lambda function not in a VPC", () => {
+  // Stack
+  const stack = new Stack();
+
+  const testLambdaFunction = new lambda.Function(stack, 'test-lamba', {
+    runtime: lambda.Runtime.NODEJS_10_X,
+    handler: "index.handler",
+    code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+  });
+
+  const testVpc = new ec2.Vpc(stack, "test-vpc", {});
+
+  // Helper declaration
+  const app = () => {
+    // Helper declaration
+    new LambdaToSecretsmanager(stack, "lambda-to-secretsmanager-stack", {
+      existingLambdaObj: testLambdaFunction,
+      existingVpc: testVpc,
+      secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+    });
+  };
+
+  // Assertion
+  expect(app).toThrowError();
+
+});
+
+// --------------------------------------------------------------
+// Test bad call with existingVpc and deployVpc
+// --------------------------------------------------------------
+test("Test bad call with existingVpc and deployVpc", () => {
+  // Stack
+  const stack = new Stack();
+
+  const testVpc = new ec2.Vpc(stack, "test-vpc", {});
+
+  const app = () => {
+    // Helper declaration
+    new LambdaToSecretsmanager(stack, "lambda-to-secretsmanager-stack", {
+      lambdaFunctionProps: {
+        runtime: lambda.Runtime.NODEJS_10_X,
+        handler: "index.handler",
+        code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+      },
+      secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+      existingVpc: testVpc,
+      deployVpc: true,
+    });
+  };
+  // Assertion
+  expect(app).toThrowError();
+});
+
+// --------------------------------------------------------------
+// Test lambda function custom environment variable
+// --------------------------------------------------------------
+test('Test lambda function custom environment variable', () => {
+  // Stack
+  const stack = new Stack();
+
+  // Helper declaration
+  new LambdaToSecretsmanager(stack, 'lambda-to-secretsmanager-stack', {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+      environment: {
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      }
+    },
+    secretProps: { removalPolicy: RemovalPolicy.DESTROY },
+    secretEnvironmentVariableName: 'CUSTOM_SECRET_NAME'
+  });
+
+  // Assertion
+  expect(stack).toHaveResource('AWS::Lambda::Function', {
+    Handler: 'index.handler',
+    Runtime: 'nodejs14.x',
+    Environment: {
+      Variables: {
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        CUSTOM_SECRET_NAME: {
+          Ref: 'lambdatosecretsmanagerstacksecretBA684E34'
+        }
+      }
+    }
+  });
+});
+
+// --------------------------------------------------------------
+// Test overriding secretProps to pass a customer provided CMK
+// --------------------------------------------------------------
+test('Test overriding secretProps to pass a customer provided CMK', () => {
+  // Stack
+  const stack = new Stack();
+
+  const encryptionKey = defaults.buildEncryptionKey(stack, {
+    description: 'secret-key'
+  });
+
+  // Helper declaration
+  new LambdaToSecretsmanager(stack, 'lambda-to-secretsmanager-stack', {
+    lambdaFunctionProps: {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+      environment: {
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      }
+    },
+    secretProps: {
+      encryptionKey
+    }
+  });
+
+  // Assertion 1
+  expect(stack).toHaveResource('AWS::Lambda::Function', {
+    Handler: 'index.handler',
+    Runtime: 'nodejs14.x',
+    Environment: {
+      Variables: {
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        SECRET_ARN: {
+          Ref: 'lambdatosecretsmanagerstacksecretBA684E34'
+        }
+      }
+    }
+  });
+
+  // Assertion 2
+  expect(stack).toHaveResource("AWS::SecretsManager::Secret", {
+    GenerateSecretString: {},
+    KmsKeyId: {
+      "Fn::GetAtt": [
+        "EncryptionKey1B843E66",
+        "Arn"
+      ]
+    }
+  });
+
+  // Assertion 3
+  expect(stack).toHaveResource('AWS::KMS::Key', {
+    KeyPolicy: {
+      Statement: [
+        {
+          Action: [
+            "kms:Create*",
+            "kms:Describe*",
+            "kms:Enable*",
+            "kms:List*",
+            "kms:Put*",
+            "kms:Update*",
+            "kms:Revoke*",
+            "kms:Disable*",
+            "kms:Get*",
+            "kms:Delete*",
+            "kms:ScheduleKeyDeletion",
+            "kms:CancelKeyDeletion",
+            "kms:GenerateDataKey",
+            "kms:TagResource",
+            "kms:UntagResource"
+          ],
+          Effect: "Allow",
+          Principal: {
+            AWS: {
+              "Fn::Join": [
+                "",
+                [
+                  "arn:",
+                  {
+                    Ref: "AWS::Partition"
+                  },
+                  ":iam::",
+                  {
+                    Ref: "AWS::AccountId"
+                  },
+                  ":root"
+                ]
+              ]
+            }
+          },
+          Resource: "*"
+        },
+        {
+          Action: [
+            "kms:Decrypt",
+            "kms:Encrypt",
+            "kms:ReEncrypt*",
+            "kms:GenerateDataKey*"
+          ],
+          Condition: {
+            StringEquals: {
+              "kms:ViaService": {
+                "Fn::Join": [
+                  "",
+                  [
+                    "secretsmanager.",
+                    {
+                      Ref: "AWS::Region"
+                    },
+                    ".amazonaws.com"
+                  ]
+                ]
+              }
+            }
+          },
+          Effect: "Allow",
+          Principal: {
+            AWS: {
+              "Fn::Join": [
+                "",
+                [
+                  "arn:",
+                  {
+                    Ref: "AWS::Partition"
+                  },
+                  ":iam::",
+                  {
+                    Ref: "AWS::AccountId"
+                  },
+                  ":root"
+                ]
+              ]
+            }
+          },
+          Resource: "*"
+        },
+        {
+          Action: [
+            "kms:CreateGrant",
+            "kms:DescribeKey"
+          ],
+          Condition: {
+            StringEquals: {
+              "kms:ViaService": {
+                "Fn::Join": [
+                  "",
+                  [
+                    "secretsmanager.",
+                    {
+                      Ref: "AWS::Region"
+                    },
+                    ".amazonaws.com"
+                  ]
+                ]
+              }
+            }
+          },
+          Effect: "Allow",
+          Principal: {
+            AWS: {
+              "Fn::Join": [
+                "",
+                [
+                  "arn:",
+                  {
+                    Ref: "AWS::Partition"
+                  },
+                  ":iam::",
+                  {
+                    Ref: "AWS::AccountId"
+                  },
+                  ":root"
+                ]
+              ]
+            }
+          },
+          Resource: "*"
+        },
+        {
+          Action: "kms:Decrypt",
+          Condition: {
+            StringEquals: {
+              "kms:ViaService": {
+                "Fn::Join": [
+                  "",
+                  [
+                    "secretsmanager.",
+                    {
+                      Ref: "AWS::Region"
+                    },
+                    ".amazonaws.com"
+                  ]
+                ]
+              }
+            }
+          },
+          Effect: "Allow",
+          Principal: {
+            AWS: {
+              "Fn::GetAtt": [
+                "lambdatosecretsmanagerstackLambdaFunctionServiceRole035B2C55",
+                "Arn"
+              ]
+            }
+          },
+          Resource: "*"
+        }
+      ],
+      Version: "2012-10-17"
+    },
+    Description: "secret-key",
+    EnableKeyRotation: true
+  });
+});
