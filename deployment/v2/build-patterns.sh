db56b0288cf0b0cc6b9b2d18c334137590a3a0db
Hitendra Nishar <hnishar@amazon.com>
2021-09-10 08:07:00 -0400

    feat(cdk-v2): separating out rewrite import code into standalone script

--- deployment/v2/build-patterns.sh
+++ deployment/v2/build-patterns.sh
@@ -9,8 +9,12 @@ echo "aligning versions and updating package.json for CDK v2..."
 /bin/bash $deployment_dir/align-version.sh
 
 echo "============================================================================================="
-echo "building aws-cdk-migration..."
-cd $source_dir/tools/aws-cdk-migration
+echo "updating Import statements for CDK v2..."
+/bin/bash $deployment_dir/rewrite-imports.sh
+
+echo "============================================================================================="
+echo "building cdk-integ-tools..."
+cd $source_dir/tools/cdk-integ-tools
 npm install
 npm run build
 npm link
@@ -21,23 +25,6 @@ cd $source_dir/
 
 export PATH=$(npm bin):$PATH
 export NODE_OPTIONS="--max-old-space-size=4096 ${NODE_OPTIONS:-}"
-
-echo "============================================================================================="
-echo "Migrating TypeScript import statements from modular CDK (i.e. @aws-cdk/aws-s3) to aws-cdk-lib (i.e. aws-cdk-lib)"
-cd $source_dir/
-for subdir in $source_dir/patterns/\@aws-solutions-constructs/* ; do
-  if [ -d "$subdir" -a `basename $subdir` != "node_modules" ]; then
-    echo $subdir
-    rewrite-imports-v2 $subdir/**/*.ts
-  fi
-done
-
-echo "============================================================================================="
-echo "building cdk-integ-tools..."
-cd $source_dir/tools/cdk-integ-tools
-npm install
-npm run build
-npm link
 
 echo "============================================================================================="
 echo "installing..."
@@ -56,5 +43,5 @@ echo "reverting back versions and updates to package.json for CDK v2..."
 /bin/bash $deployment_dir/align-version.sh revert
 
 echo "============================================================================================="
-echo "reverting back TypeScript import statements for CDK v2..."
-git checkout `find . -name *.ts | grep -v node_modules | grep -v **/*.d.ts`
+echo "reverting back Import statements for CDK v2..."
+/bin/bash $deployment_dir/rewrite-imports.sh revert
