66ebb5dab5bbfb796fdaee5df4f984b76f79265b
mickychetta <cmicky@amazon.com>
2021-09-13 17:05:29 -0700

    delete trailing whitespace

--- source/patterns/@aws-solutions-constructs/aws-dynamodb-stream-lambda-elasticsearch-kibana/lib/index.ts
+++ source/patterns/@aws-solutions-constructs/aws-dynamodb-stream-lambda-elasticsearch-kibana/lib/index.ts
@@ -119,7 +119,7 @@ export class DynamoDBStreamToLambdaToElasticSearchAndKibana extends Construct {
     const convertedProps: DynamoDBStreamToLambdaToElasticSearchAndKibanaProps = { ...props };
 
     // W (for 'wrapped') is added to the id so that the id's of the constructs with the old and new names don't collide
-    // If this character pushes you beyond the 64 character limit, just import the new named construct and instantiate 
+    // If this character pushes you beyond the 64 character limit, just import the new named construct and instantiate
     // it in place of the older named version. They are functionally identical, aside from the types no other changes
     // will be required.  (eg - new DynamoDBStreamsToLambdaToElasticSearchAndKibana instead of DynamoDBStreamToLambdaToElasticSearchAndKibana)
     const wrappedConstruct: DynamoDBStreamToLambdaToElasticSearchAndKibana = new DynamoDBStreamsToLambdaToElasticSearchAndKibana(
