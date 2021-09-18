3babcb51910ac09afafedfeea3227694ec2563a5
urukonda <urukonda>
2021-09-19 01:28:24 +0530

    implement aws-iot-kinesisstreams construct

---
+++ source/patterns/@aws-solutions-constructs/aws-iot-kinesisstreams/test/integ.new-kinesisstream.ts
@@ -0,0 +1,43 @@
+/**
+ *  Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+// Imports
+import { App, Stack, Duration } from "@aws-cdk/core";
+import { IotToKinesisStreams, IotToKinesisStreamsProps } from "../lib";
+import { generateIntegStackName } from '@aws-solutions-constructs/core';
+
+// Setup
+const app = new App();
+const stack = new Stack(app, generateIntegStackName(__filename));
+stack.templateOptions.description = 'Integration Test for aws-iot-kinesisstreams';
+
+// Definitions
+const props: IotToKinesisStreamsProps = {
+  iotTopicRuleProps: {
+    topicRulePayload: {
+      description: "sends data to kinesis streams",
+      sql: "SELECT * FROM 'solutions/constructs'",
+      actions: []
+    },
+  },
+  kinesisStreamProps: {
+    shardCount: 5,
+    streamName: 'testnewstream',
+    retentionPeriod: Duration.hours(30)
+  }
+};
+
+new IotToKinesisStreams(stack, 'test-iot-kinesisstreams', props);
+
+// Synth
+app.synth();
