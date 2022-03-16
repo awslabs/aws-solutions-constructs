860e6bb221cb9e0adb93d48aae9e0f42ed193be1
biffgaut <78155736+biffgaut@users.noreply.github.com>
2022-03-16 17:15:02 -0400

    Change cdk-i nteg directive

--- source/patterns/@aws-solutions-constructs/aws-cloudfront-mediastore/test/integ.customCloudFrontLoggingBucket.ts
+++ source/patterns/@aws-solutions-constructs/aws-cloudfront-mediastore/test/integ.customCloudFrontLoggingBucket.ts
@@ -11,7 +11,6 @@
  *  and limitations under the License.
  */
 
-/// !cdk-integ *
 import { App, Stack, RemovalPolicy } from "@aws-cdk/core";
 import { CloudFrontToMediaStore } from "../lib";
 import { BucketEncryption } from "@aws-cdk/aws-s3";
