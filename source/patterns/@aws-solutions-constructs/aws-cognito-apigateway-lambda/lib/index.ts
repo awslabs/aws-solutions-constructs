c2f9d846837ffc28160a70b3368b373c6eefb4f6
biffgaut <biffgaut@amazon.com>
2023-02-14 16:09:34 -0500

    One more reference to API Helper function

--- source/patterns/@aws-solutions-constructs/aws-cognito-apigateway-lambda/lib/index.ts
+++ source/patterns/@aws-solutions-constructs/aws-cognito-apigateway-lambda/lib/index.ts
@@ -105,8 +105,11 @@ export class CognitoToApiGatewayToLambda extends Construct {
       existingLambdaObj: props.existingLambdaObj,
       lambdaFunctionProps: props.lambdaFunctionProps
     });
-    [this.apiGateway, this.apiGatewayCloudWatchRole, this.apiGatewayLogGroup] =
-      defaults.GlobalLambdaRestApi(this, this.lambdaFunction, props.apiGatewayProps, props.logGroupProps);
+    const restApi = defaults.GlobalLambdaRestApi(this, this.lambdaFunction, props.apiGatewayProps, props.logGroupProps);
+    this.apiGateway = restApi.api;
+    this.apiGatewayCloudWatchRole = restApi.role;
+    this.apiGatewayLogGroup = restApi.group;
+
     this.userPool = defaults.buildUserPool(this, props.cognitoUserPoolProps);
     this.userPoolClient = defaults.buildUserPoolClient(this, this.userPool, props.cognitoUserPoolClientProps);
 
