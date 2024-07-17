56795508b0841ad7596e9c3f0247c0c1dbfbbec4
biffgaut <biffgaut@amazon.com>
2024-07-17 13:21:47 -0400

    Split up build steps

---
+++ deployment/v2/build-all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[$PWD!=aws-solutions-constructs]]
+then
+  echo Script must be run from aws-solutions-constructs folder
+  exit 1
+fi
+
+deployment_dir=$(cd $(dirname $0) && pwd)
+source_dir="$deployment_dir/../../source"
+
+echo "============================================================================================="
+echo "aligning versions and updating package.json for CDK v2..."
+/bin/bash $deployment_dir/align-version.sh
+
+bail="--bail"
+runtarget="build+lint+test"
+cd $source_dir/
+
+export PATH=$source_dir/node_modules/.bin:$PATH
+export NODE_OPTIONS="--max-old-space-size=4096 ${NODE_OPTIONS:-}"
+
+echo "============================================================================================="
+echo "building..."
+time lerna run $bail --stream $runtarget || fail
+
+echo "============================================================================================="
+echo "packaging..."
+time lerna run --bail --stream jsii-pacmak || fail
+
+echo "============================================================================================="
+echo "reverting back versions and updates to package.json for CDK v2..."
+/bin/bash $deployment_dir/align-version.sh revert
--- deployment/v2/build-patterns.sh
+++ deployment/v2/build-patterns.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+if [[$PWD!=aws-solutions-constructs]]
+then
+  echo Script must be run from aws-solutions-constructs folder
+  exit 1
+fi
+
 deployment_dir=$(cd $(dirname $0) && pwd)
-source_dir="$deployment_dir/../../source"
 
-echo "============================================================================================="
-echo "aligning versions and updating package.json for CDK v2..."
-/bin/bash $deployment_dir/align-version.sh
+source $deployment_dir/bootstrap.sh
 
-bail="--bail"
-runtarget="build+lint+test"
-cd $source_dir/
-
-export PATH=$source_dir/node_modules/.bin:$PATH
-export NODE_OPTIONS="--max-old-space-size=4096 ${NODE_OPTIONS:-}"
-
- npm install -g @aws-cdk/integ-runner
-
-echo "============================================================================================="
-echo "installing..."
-yarn install --frozen-lockfile
-
-echo "============================================================================================="
-echo "building..."
-time lerna run $bail --stream $runtarget || fail
-
-echo "============================================================================================="
-echo "packaging..."
-time lerna run --bail --stream jsii-pacmak || fail
-
-echo "============================================================================================="
-echo "reverting back versions and updates to package.json for CDK v2..."
-/bin/bash $deployment_dir/align-version.sh revert
+source $deployment_dir/build-all.sh
