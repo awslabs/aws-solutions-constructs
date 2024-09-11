b1baf59c13d6270cd1e32dcaed7dae3c6b302087
Jonathan Barnett <me@indieisaconcept.com>
2024-09-11 12:07:54 +1000

    feat(aws-openapigateway-lambda): update construct to allow specifying an inline api definition (#1190)
    * feat(aws-openapigateway-lambda): update construct to allow specifying an inline api definition
    
    The rationale for this change is to improve the local developer experience and enable compatibility with `sam local start-api`. Previously, the construct only supported referencing an external OpenAPI definition file, which made it challenging to work with an API locally during development.
    
    * fix: amend typo in comment
    
    * Constructs Team Updates
    
    * Final edits (new tests, var name changes)
    
    * Sync README
    
    * Change name back
    
    * Comment on the export
    
    * Change name to apiDefinitionJson
    
    * Address comments
    
    * trailing whitespace
    
    * Build tweaks
    
    ---------
    
    Co-authored-by: Jonathan Barnett <barnttjm@amazon.com>
    Co-authored-by: biffgaut <biffgaut@amazon.com>
    Co-authored-by: AWS Solutions Constructs Team <67720492+aws-solutions-constructs-team@users.noreply.github.com>

--- source/patterns/@aws-solutions-constructs/aws-openapigateway-lambda/lib/index.ts
+++ source/patterns/@aws-solutions-constructs/aws-openapigateway-lambda/lib/index.ts
@@ -11,58 +11,18 @@
  *  and limitations under the License.
  */
 
-import { Aws } from 'aws-cdk-lib';
 import * as cdk from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as defaults from '@aws-solutions-constructs/core';
-import * as resources from '@aws-solutions-constructs/resources';
 import { RestApiBaseProps } from 'aws-cdk-lib/aws-apigateway';
 import { Asset } from 'aws-cdk-lib/aws-s3-assets';
-
-/**
- * The ApiIntegration interface is used to correlate a user-specified id with either a existing lambda function or set of lambda props.
- *
- * See the 'Overview of how the OpenAPI file transformation works' section of the README.md for more details on its usage.
- */
-export interface ApiIntegration {
-  /**
-   * Id of the ApiIntegration, used to correlate this lambda function to the api integration in the open api definition.
-   *
-   * Note this is not a CDK Construct ID, and is instead a client defined string used to map the resolved lambda resource with the OpenAPI definition.
-   */
-  readonly id: string;
-  /**
-   * The Lambda function to associate with the API method in the OpenAPI file matched by id.
-   *
-   * One and only one of existingLambdaObj or lambdaFunctionProps must be specified, any other combination will cause an error.
-   */
-  readonly existingLambdaObj?: lambda.Function;
-  /**
-   * Properties for the Lambda function to create and associate with the API method in the OpenAPI file matched by id.
-   *
-   * One and only one of existingLambdaObj or lambdaFunctionProps must be specified, any other combination will cause an error.
-   */
-  readonly lambdaFunctionProps?: lambda.FunctionProps;
-}
-
-/**
- * Helper object to map an ApiIntegration id to its resolved lambda.Function. This type is exposed as a property on the instantiated construct.
- */
-export interface ApiLambdaFunction {
-  /**
-   * Id of the ApiIntegration, used to correlate this lambda function to the api integration in the open api definition.
-   */
-  readonly id: string;
-  /**
-   * The instantiated lambda.Function.
-   */
-  readonly lambdaFunction: lambda.Function;
-}
+import { ApiIntegration, CheckOpenApiProps, ApiLambdaFunction, ObtainApiDefinition } from './openapi-helper';
+// openapi-helper is on its way to core, so these interfaces must be exported here
+export { ApiIntegration, ApiLambdaFunction } from './openapi-helper';
 
 export interface OpenApiGatewayToLambdaProps {
   /**
@@ -77,6 +37,13 @@ export interface OpenApiGatewayToLambdaProps {
    * Local file asset of the OpenAPI spec file.
    */
   readonly apiDefinitionAsset?: Asset;
+  /**
+   * OpenAPI specification represented in a JSON object to be embedded in the CloudFormation template.
+   * IMPORTANT - Including the spec in the template introduces a risk of the template growing too big, but
+   * there are some use cases that require an embedded spec. Unless your use case explicitly requires an embedded spec
+   * you should pass your spec as an S3 asset.
+   */
+  readonly apiDefinitionJson?: any;
   /**
    * One or more key-value pairs that contain an id for the api integration
    * and either an existing lambda function or an instance of the LambdaProps.
@@ -144,10 +111,7 @@ export class OpenApiGatewayToLambda extends Construct {
 
   constructor(scope: Construct, id: string, props: OpenApiGatewayToLambdaProps) {
     super(scope, id);
-    CheckOpenapiProps(props);
-
-    const apiDefinitionBucket = props.apiDefinitionBucket ?? props.apiDefinitionAsset?.bucket;
-    const apiDefinitionKey = props.apiDefinitionKey ?? props.apiDefinitionAsset?.s3ObjectKey;
+    CheckOpenApiProps(props);
 
     // store a counter to be able to uniquely name lambda functions to avoid naming collisions
     let lambdaCounter = 0;
@@ -169,45 +133,27 @@ export class OpenApiGatewayToLambda extends Construct {
       }
     });
 
-    // Map each id and lambda function pair to the required format for the template writer custom resource
-    const apiIntegrationUris = this.apiLambdaFunctions.map(apiLambdaFunction => {
-      // the placeholder string that will be replaced in the OpenAPI Definition
-      const uriPlaceholderString = apiLambdaFunction.id;
-      // the endpoint URI of the backing lambda function, as defined in the API Gateway extensions for OpenAPI here:
-      // https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-swagger-extensions-integration.html
-      const uriResolvedValue = `arn:${Aws.PARTITION}:apigateway:${Aws.REGION}:lambda:path/2015-03-31/functions/${apiLambdaFunction.lambdaFunction.functionArn}/invocations`;
-
-      return {
-        id: uriPlaceholderString,
-        value: uriResolvedValue
-      };
-    });
-
-    // This custom resource will overwrite the string placeholders in the openapi definition with the resolved values of the lambda URIs
-    const apiDefinitionWriter = resources.createTemplateWriterCustomResource(this, 'Api', {
-      // CheckAlbProps() has confirmed the existence of these values
-      templateBucket: apiDefinitionBucket!,
-      templateKey: apiDefinitionKey!,
-      templateValues: apiIntegrationUris,
-      timeout: props.internalTransformTimeout ?? cdk.Duration.minutes(1),
-      memorySize: props.internalTransformMemorySize ?? 1024
+    const definition = ObtainApiDefinition(this,  {
+      tokenToFunctionMap: this.apiLambdaFunctions,
+      apiDefinitionBucket: props.apiDefinitionBucket,
+      apiDefinitionKey: props.apiDefinitionKey,
+      apiDefinitionAsset: props.apiDefinitionAsset,
+      apiJsonDefinition: props.apiDefinitionJson,
+      internalTransformTimeout: props.internalTransformTimeout,
+      internalTransformMemorySize: props.internalTransformMemorySize
     });
 
     const specRestApiResponse = defaults.CreateSpecRestApi(this, {
       ...props.apiGatewayProps,
-      apiDefinition: apigateway.ApiDefinition.fromBucket(
-        apiDefinitionWriter.s3Bucket,
-        apiDefinitionWriter.s3Key
-      )
+      apiDefinition: definition
     }, props.logGroupProps);
 
     this.apiGateway = specRestApiResponse.api;
     this.apiGatewayCloudWatchRole = specRestApiResponse.role;
     this.apiGatewayLogGroup = specRestApiResponse.logGroup;
 
-    // Redeploy the API any time the incoming API definition changes (from asset or s3 object)
-    this.apiGateway.latestDeployment?.addToLogicalId(apiDefinitionKey);
-
+    // Redeploy the API any time a decoupled (non-inline) API definition changes (from asset or s3 object)
+    this.apiGateway.latestDeployment?.addToLogicalId(props.apiDefinitionKey ?? props.apiDefinitionAsset?.s3ObjectKey);
     this.apiLambdaFunctions.forEach(apiLambdaFunction => {
       // Redeploy the API any time one of the lambda functions changes
       this.apiGateway.latestDeployment?.addToLogicalId(apiLambdaFunction.lambdaFunction.functionArn);
@@ -218,33 +164,4 @@ export class OpenApiGatewayToLambda extends Construct {
       });
     });
   }
-}
-
-function CheckOpenapiProps(props: OpenApiGatewayToLambdaProps) {
-
-  let errorMessages = '';
-  let errorFound = false;
-
-  if (props.apiDefinitionAsset && (props.apiDefinitionBucket || props.apiDefinitionKey)) {
-    errorMessages += 'Either apiDefinitionBucket/apiDefinitionKey or apiDefinitionAsset must be specified, but not both\n';
-    errorFound = true;
-  }
-
-  const apiDefinitionBucket = props.apiDefinitionBucket ?? props.apiDefinitionAsset?.bucket;
-  const apiDefinitionKey = props.apiDefinitionKey ?? props.apiDefinitionAsset?.s3ObjectKey;
-
-  if (apiDefinitionBucket === undefined || apiDefinitionKey === undefined) {
-    errorMessages += 'Either apiDefinitionBucket/apiDefinitionKey or apiDefinitionAsset must be specified\n';
-    errorFound = true;
-  }
-
-  if (props.apiIntegrations === undefined || props.apiIntegrations.length < 1) {
-    errorMessages += 'At least one ApiIntegration must be specified in the apiIntegrations property\n';
-    errorFound = true;
-  }
-
-  if (errorFound) {
-    throw new Error(errorMessages);
-  }
-
 }
