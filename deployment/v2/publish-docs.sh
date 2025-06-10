22929d80af0616e29cdccde15d3afbd78d7c49e9
biffgaut <biffgaut@amazon.com>
2025-06-10 11:25:06 -0400

    image work

--- deployment/v2/publish-docs.sh
+++ deployment/v2/publish-docs.sh
@@ -39,9 +39,9 @@ find source/patterns -name "README.adoc" -type f | while read file; do
     cp "$file" "documentation/${parent_dir}.adoc"
     cp "${dir_name}/${parent_dir}.png" "documentation/images/"
 
-    perl -i -0pe "s/© Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\n/\n/g" "documentation/${parent_dir}.adoc"
+    # perl -i -0pe "s/© Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\n/\n/g" "documentation/${parent_dir}.adoc"
 
-    perl -i -pe "s/^image::aws-/image::images\aws-/g" "documentation/${parent_dir}.adoc"
+    perl -i -pe "s/^image::aws-/image::images\/aws-/g" "documentation/${parent_dir}.adoc"
 
     perl -i -pe "s/^\/\/ github block/\[[github,topic.title]]\n== GitHub\n\n[cols="1,1", options="header"\]\n|===\n| \nTo view the code for this pattern, create\/view issues and pull requests, and more:\n\n\n\n|\n\n\n\nimage::images\/GitHub-Mark-32px.png\[The github logo.,scaledwidth=100%]\n\n|https:\/\/github.com\/awslabs\/aws-solutions-constructs\/tree\/master\/source\/patterns\/%40aws-solutions-constructs\/aws-${parent_dir}[@aws-solutions-constructs${parent_dir}]\n\|===/g" "documentation/${parent_dir}.adoc"
 
