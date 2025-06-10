b6f357a01a12afdf327bbc2a6471c963148b6924
biffgaut <biffgaut@amazon.com>
2025-06-10 11:01:52 -0400

    Internal edits to published adocs

--- deployment/v2/publish-docs.sh
+++ deployment/v2/publish-docs.sh
@@ -2,7 +2,10 @@
 set -e
 set -o xtrace
 
-# This script copies all the Construct README.adoc files from their folders
+# This script updates the documentation folder with the latest info from
+# README.adoc and architecture diagrams in the Constructs folders.
+#
+# To do so, it copies all the Construct README.adoc files from their folders
 # under @aws-solutions-constructs to the documentation, updating the name to
 # the name of the construct. Then it copies all the architecture .png files
 # to documenation/images. It then runs perl scripts to
@@ -35,5 +38,11 @@ find source/patterns -name "README.adoc" -type f | while read file; do
     # Copy the file with the new name
     cp "$file" "documentation/${parent_dir}.adoc"
     cp "${dir_name}/${parent_dir}.png" "documentation/images/"
+
+    perl -i -0pe "s/© Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\n/\n/g" "documentation/${parent_dir}.adoc"
+
+    perl -i -pe "s/^image::aws-/image::images\aws-/g" "documentation/${parent_dir}.adoc"
+
+    perl -i -pe "s/^\/\/ github block/\[[github,topic.title]]\n== GitHub\n\n[cols="1,1", options="header"\]\n|===\n| \nTo view the code for this pattern, create\/view issues and pull requests, and more:\n\n\n\n|\n\n\n\nimage::images\/GitHub-Mark-32px.png\[The github logo.,scaledwidth=100%]\n\n|https:\/\/github.com\/awslabs\/aws-solutions-constructs\/tree\/master\/source\/patterns\/%40aws-solutions-constructs\/aws-${parent_dir}[@aws-solutions-constructs${parent_dir}]\n\|===/g" "documentation/${parent_dir}.adoc"
 
 done
