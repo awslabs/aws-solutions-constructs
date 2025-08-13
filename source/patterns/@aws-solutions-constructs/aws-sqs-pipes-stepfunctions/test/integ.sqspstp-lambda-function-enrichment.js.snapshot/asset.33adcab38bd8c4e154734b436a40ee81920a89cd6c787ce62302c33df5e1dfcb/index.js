c4337763eeca3b8962c231349ea5683f05febe03
biffgaut <biffgaut@amazon.com>
2025-08-13 08:11:37 -0400

    Remove asset.*. folders

--- source/patterns/@aws-solutions-constructs/aws-sqs-pipes-stepfunctions/test/integ.sqspstp-lambda-function-enrichment.js.snapshot/asset.33adcab38bd8c4e154734b436a40ee81920a89cd6c787ce62302c33df5e1dfcb/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => { 
-  console.log(event); 
-  const response = event.map((x) =>{
-    const body = JSON.parse(x.body);
-    body.newAttrib = "content";
-    return body;
-  });
-  return response;
-}
