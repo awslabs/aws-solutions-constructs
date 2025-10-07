cc313252cad706dcc84ba1e2b0ae760e332dfce2
biffgaut <78155736+biffgaut@users.noreply.github.com>
2025-10-07 07:12:48 -0400

    feat(aws-cloudfront-s3): add origin to additionalBehaviors when not specified (#1417)
    * Add origin to additionalBehaviors when not specified
    
    * Update the docs
    
    * remove ".only" directive
    
    * Remove filename from OAC physical name
    
    * Address PR Comments

---
+++ source/patterns/@aws-solutions-constructs/aws-cloudfront-s3/test/integ.cfts3-additional-behavior.ts
@@ -0,0 +1,94 @@
+/**
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance
+ *  with the License. A copy of the License is located at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES
+ *  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
+// Imports
+import { App, Stack, RemovalPolicy } from "aws-cdk-lib";
+import { CloudFrontToS3 } from "../lib";
+import { generateIntegStackName, SetConsistentFeatureFlags } from '@aws-solutions-constructs/core';
+import { IntegTest } from '@aws-cdk/integ-tests-alpha';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as defaults from '@aws-solutions-constructs/core';
+import { CachePolicy, CfnOriginAccessControl } from "aws-cdk-lib/aws-cloudfront";
+
+// Setup
+const app = new App();
+const stack = new Stack(app, generateIntegStackName(__filename));
+stack.templateOptions.description = 'Integration Test for aws-cloudfront-s3';
+SetConsistentFeatureFlags(stack);
+
+const additionalBucket = defaults.CreateScrapBucket(stack, "scrapBucket", {
+  removalPolicy: RemovalPolicy.DESTROY,
+  autoDeleteObjects: true,
+
+});
+
+const originAccessControl = new CfnOriginAccessControl(stack, 'CloudFrontOac', {
+  originAccessControlConfig: {
+    name: defaults.generatePhysicalOacName('aws-cloudfront-s3-', ["spare"]),
+    originAccessControlOriginType: 's3',
+    signingBehavior: 'always',
+    signingProtocol: 'sigv4',
+    description: 'Origin access control provisioned by aws-cloudfront-s3'
+  }
+});
+
+const additionalOrigin = new defaults.S3OacOrigin(additionalBucket, {
+  originAccessControl
+});
+
+const construct = new CloudFrontToS3(stack, 'test-cloudfront-s3', {
+  cloudFrontLoggingBucketProps: {
+    removalPolicy: RemovalPolicy.DESTROY,
+    autoDeleteObjects: true
+  },
+  bucketProps: {
+    removalPolicy: RemovalPolicy.DESTROY,
+    autoDeleteObjects: true
+  },
+  loggingBucketProps: {
+    removalPolicy: RemovalPolicy.DESTROY,
+    autoDeleteObjects: true
+  },
+  cloudFrontLoggingBucketAccessLogBucketProps: {
+    removalPolicy: RemovalPolicy.DESTROY,
+    autoDeleteObjects: true
+  },
+  cloudFrontDistributionProps: {
+    additionalBehaviors: {
+      '/assets/public/*': {
+        origin: additionalOrigin,
+        cachePolicy: CachePolicy.CACHING_DISABLED,
+      },
+      'ngsw.json': {
+        cachePolicy: CachePolicy.CACHING_DISABLED,
+      }
+    }
+  }
+});
+
+const s3Bucket = construct.s3Bucket as s3.Bucket;
+
+defaults.addCfnSuppressRules(s3Bucket, [
+  {
+    id: 'W35',
+    reason: 'This S3 bucket is created for unit/ integration testing purposes only.'
+  },
+]);
+
+defaults.suppressCustomHandlerCfnNagWarnings(stack, 'Custom::S3AutoDeleteObjectsCustomResourceProvider');
+// Synth
+new IntegTest(stack, 'Integ', {
+  testCases: [
+    stack
+  ]
+});
